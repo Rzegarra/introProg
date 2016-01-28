b91fdbb691c8e79e136de3ea1d22e1617aa8f888
Rzegarra <Rafael.zegarra@ucsp.edu.pe>
2016-01-28 15:10:50 -0500

    28/01/2016
    aqui estan las clases avanzAdas y algunos ejemplos , tambien estan los
    archivos base apra poder practicar en javascript.

---
+++ clases/clases.js
@@ -0,0 +1,16 @@
+// var temp=17;
+// var numero2=15;
+// var resultado=numero2*temp;
+// alert('resultado= ' +resultado);
+// console.log('resultado= ' + resultado)
+//sumar todos los numeros impares de este vector
+//[123,45,79,76,12,41,7859,41,51,269,845123,45,79,76,12,41,7859,41,51,269];
+
+
+var resultado=[];
+var vec = [1,4,5,9,2,1,4]
+var tam = vec.length; 
+for (var i=0 ; i<tam ; i++){
+	resultado[i]=1;
+}
+console.log (resultado)
---
+++ clases/ejemplos.js
@@ -0,0 +1,82 @@
+var conteo=0
+if(0==0){
+	var conteo = conteo * 9;
+}
+else{
+	conteo+=5;
+}
+-------> conteo = 0
+
+var conteo=0
+if('0'=='O'){
+	var conteo=54%2;
+}
+else{
+	conteo*=5;
+}
+-------> conteo = 0
+
+var conteo=10
+if(['s','t']=='st'){
+	var conteo++;
+}
+else{
+	conteo/=5;
+}
+-------> conteo = 0
+var conteo=20
+if(89==89){
+	var conteo%=5;
+}
+else{
+	conteo+=5;
+}
+-------> conteo = 0
+
+var conteo=10
+if(5*6==31-1){
+	var conteo-=89;
+}
+else{
+	conteo-=5;
+}
+-------> conteo = 0
+
+var conteo=2
+if(8548756%2==0){
+	var conteo+=15;
+}
+else{
+	conteo=conteo/2;
+}
+-------> conteo = 0
+
+var conteo=2
+if(5454521%2==0){
+	var conteo+=9;
+}
+else{
+	conteo+=5;
+}
+-------> conteo = 0
+
+var conteo=0
+if(0==conteo){
+	var conteo--;
+}
+else{
+	conteo+=5;
+}
+-------> conteo = 0
+
+var conteo=0
+if(8==conteo+=3){
+	var conteo*=0;
+}
+-------> conteo = 0
+
+var conteo=0
+if((true && false)!=false){
+	var conteo+=9;
+}
+-------> conteo = 0
