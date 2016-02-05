34df41a984e3ad8bf33fe5844fde372dd3a5eb61
Rzegarra <Rafael.zegarra@ucsp.edu.pe>
2016-02-05 17:57:51 -0500

    clases 02-05

--- clases/clases.js
+++ clases/clases.js
@@ -112,34 +112,63 @@
 /////////////////////////////
 ////////////////////////////
 
-var vec1=[1,2,5,4];
-var vec2=[2,4,5,4,7];
-var tam1=vec1.length;
-var tam2=vec2.length;
-var cantidad=0;
-var mayor;
-if (tam1>tam2) 
-	mayor=tam1;
-else
-	mayor=tam2;
-for (var i = mayor - 1; i >= 0; i--) {
-	if (vec1[i]==4) {
-		cantidad++
-	};
-	if (vec2[i]==4) {
-		cantidad--
+
+// var vec1=[1,2,5,4];
+// var vec2=[2,4,5,4,07];
+// var tam1=vec1.length;
+// var tam2=vec2.length;
+// var cantidad=0;
+// var mayor;
+// if (tam1>tam2) 
+// 	mayor=tam1;
+// else
+// 	mayor=tam2;
+// for (var i = mayor - 1; i >= 0; i--) {
+// 	if (vec1[i]==4) {
+// 		cantidad++
+// 	};
+// 	if (vec2[i]==4) {
+// 		cantidad--
+// 	};
+// };
+// if (cantidad>0){
+// 	console.log('el q ue tiene mas 4s es = '+ vec1);
+// }
+// else if (cantidad==0){
+// 	console.log('ambos tienen la misma cantidad de 4s');
+// }
+// else{
+// 	console.log('el q ue tiene mas 4s es = '+ vec2);
+// }
+//de los vectores hallar  la suma de todos los numeros que contengan.
+
+// var vec1=[1,2];
+// var vec2=[1,1,1];
+// var unidos=vec1.concat(vec2);
+//[1,2,1,1,1]
+// 0 1 2 3 4
+ //1+1
+ //2+1
+ //3+1
+ //2+4
+
+// for (var i = unidos.length - 1; i > 0; i--) {
+// 	unidos[0]+=unidos[i];
+// };
+// console.log('la suma de todos es = ' + unidos[0])
+
+
+//mostrar la cantidad de 1s que existen en los vectores
+var vec1=[1,2];
+var vec2=[1,1,1];
+var temp=0;
+var unidos=vec1.concat(vec2);
+for (var i = unidos.length - 1; i >= 0; i--) {
+	if (unidos[i]==1) {
+		temp++;
 	};
 };
-if (cantidad>0){
-	console.log('el q ue tiene mas 4s es = '+ vec1);
-}
-else if (cantidad==0){
-	console.log('ambos tienen la misma cantidad de 4s');
-}
-else{
-	console.log('el q ue tiene mas 4s es = '+ vec2);
-}
-//de los vectores hallar  la suma de todos los numeros que contengan.
+console.log('existen '+ temp + ' digitos iguales a 1')
 
 //de los vectores, multiplicar uno a uno cada valor que tengan y dart la suma del total.
 //se recomienda usar dos (for) ---- >      for(){
